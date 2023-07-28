223fcd27c31bfd0f172d3b1132977688126004c0
zarkuslol <lucasferlima8@gmail.com>
2023-07-28 16:53:44 -0300

    Index page created and started new user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+
   root 'users#index'
 
   get '/users', to: 'users#index'
+  get '/create', to: 'users#new'
+
+  post '/users/create', to: 'users#create'
 end
