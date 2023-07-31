b4e33d43a1e17cf6dfdbfe2d1dd569f4021416a3
zarkuslol <lucasferlima8@gmail.com>
2023-07-31 18:00:08 -0300

    Root upgraded and POST /create working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to :root
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :telephone, :birth_date)
+    end
 end
